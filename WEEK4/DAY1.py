6e1258c7e565ac951e6a9797f13cd2277790bbeb
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-14 14:29:15 +0900

    코테준비(파이썬) - 4주차/DAY1

--- WEEK4/DAY1.py
+++ WEEK4/DAY1.py
@@ -2,10 +2,8 @@ H, M= map(int, input().split())
 
 if(H <= 24 and M <= 59) :
     if (H >= 1 and M < 45):
-        H = H - 1
-        print("%d %d" % (H, M - 45 + 60))
+        print("%d %d" % (H-1, M - 45 + 60))
     elif (H == 0 and M < 45):
-        H = H - 1 + 24
-        print("%d %d" % (H, M - 45 + 60))
+        print("%d %d" % (H-1+24, M - 45 + 60))
     elif(M >= 45):
         print("%d %d" % (H, M - 45))
