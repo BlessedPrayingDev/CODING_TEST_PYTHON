667d63707bb76c092a9b353c98ace6058843418f
BlessedPrayingDevGru <gasaret@gmail.com>
2023-08-16 16:23:46 +0900

    코테준비(파이썬) - 4주차/DAY2

---
+++ WEEK4/DAY2.py
@@ -0,0 +1,12 @@
+a, b= map(int, input().split())
+c = int(input())
+
+if(b+c < 60) :
+    print(a, b+c)
+elif(b+c >= 60) :
+    d = 0
+    d = (b+c) // 60
+    if((a+d) >= 24) :
+        print((a+d) % 24, (b + c) % 60)
+    else :
+        print(a+d, (b+c) % 60)
