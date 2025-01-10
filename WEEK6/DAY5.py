a12fdf9152b08df551dbca45c71675600aba8a15
BlessedPrayingDev <gasaret@gmail.com>
2025-01-10 13:39:38 +0900

    25.01.10(금) 문제 추가

---
+++ WEEK6/DAY5.py
@@ -0,0 +1,10 @@
+# 문제 번호: 10951
+# 제목: A+B -4
+# 날짜: 25.01.10(금)
+import sys
+while True:
+    try :
+        A, B = map(int,sys.stdin.readline().split())
+        print(A+B)
+    except :
+        break
